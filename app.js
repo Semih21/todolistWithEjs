c9d053dc68856372b511e3cfa5a6417ce452e1eb
SK01317 <semih.kasimoglu@randstadgroep.nl>
2019-11-15 16:59:28 +0100

    todolist is added

--- app.js
+++ app.js
@@ -4,50 +4,21 @@ const bodyParser=require('body-parser')
 const app=express()
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended:true}))
-
+var items=["Buy food","Eat food","Cook food"]
 app.get('/',(req,res)=>{
 
     var today=new Date()
-    var currentDay=today.getDay()
     var day=""
+    var options = { weekday: 'long',  month: 'long', day: 'numeric' };
+     day=today.toLocaleDateString("en-US",options)
 
-    switch(currentDay){
-        case 0:
-            day="Sunday"
-            break;
-       case 1:
-           day="Monday"
-            break;
-       case 2:
-            day="Tuesday"
-            break;
-       case 3:
-            day="Wednesday"
-            break;
-       case 4:
-             day="Thursday"
-             break;
-       case 5:
-            day="Friday"
-             break;
-       case 6:
-             day="Saturday"
-             break;
-        default :
-        console.log("Error, current day is equal to:"+currentDay)
-            
-    }
-    res.render('list',{kindOfDay:day})
-    // if(today.getDay()===6 || today.getDay()===0){
-    //     day="weekend"
-    //     res.render('list',{kindOfDay:day})
-    // //res.sendFile(__dirname+"/weekend.html")
-    // }else{
-    //     day="weekday"
-    //     // res.sendFile(__dirname+"/weekday.html")
-    //     res.render('list',{kindOfDay:day})
-    // }
-   
+    res.render('list',{kindOfDay:day,newTodoItems:items})
+  
+})
+app.post("/",(req,res)=>{
+  var item=req.body.newTodo
+   items.push(item)
+    res.redirect('/')
 })
 
 app.listen(3000,()=>{
